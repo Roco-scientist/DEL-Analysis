58cb03be64c3d5c06fc41ffdd3e84a86a5fee291
Rory Coffey <coffeyrt@gmail.com>
2021-09-12 11:26:37 -0400

    split up DelData classes into multiple types

--- src/delanalysis.py
+++ src/delanalysis.py
@@ -8,7 +8,6 @@ from typing import Optional
 class DelData:
     def __init__(self, data, zscored=False):
         self.data = data
-        self.__determine_file_type()
         self.zscored = zscored
 
     def __repr__(self):
@@ -16,12 +15,6 @@ class DelData:
 
     def __str__(self):
         self.data
-
-    def __determine_file_type(self):
-        if "Count" in self.data.columns:
-            self.file_type = "sample"
-        else:
-            self.file_type = "merged"
 
     def calculate_zscore(self, inplace=False) -> Optional[DelData]:
         if self.zscored:
@@ -41,45 +34,10 @@ class DelData:
         else:
             return DelData(zscore_df_final, zscored=True)
 
-    def reduce(self, min_score: float, inplace=False) -> Optional[DelData]:
-        if self.file_type == "merge":
-            raise Exception(
-                "Can only reduce sample data.  Either call sample_data() or input sample data")
-        if self.zscored:
-            reduced_data = self.data[self.data.zscore >= min_score]
-        else:
-            reduced_data = self.data[self.data.Count >= min_score]
-        if inplace:
-            self.data = reduced_data
-            return None
-        else:
-            return DelData(reduced_data)
 
-    def sample_data(self, sample_name: str, inplace=False) -> Optional[DelData]:
-        if self.file_type == "sample":
-            raise Exception("Data is already sample data")
-        sample_data = self.data.iloc[:, ["BB_1", "BB_2", "BB_3", sample_name]]
-        if self.zscored:
-            sample_data.rename({sample_name: "zscore"}, axis=1, inplace=True)
-        else:
-            sample_data.rename({sample_name: "count"}, axis=1, inplace=True)
-        if inplace:
-            self.data = sample_data
-            self.file_type = "sample"
-            return None
-        else:
-            return DelData(sample_data, self.zscored)
-
+class DelDataMerged(DelData):
     def quantile_normalize(self):
-        if self.file_type == "sample":
-            raise Exception("Only z-scored merged counts can be  quantile normalized")
-        if not self.zscored:
-            raise Exception("Run calculate_zscore() before trying to quatile normalize")
         # TODO finish this
-        pass
-
-    def graph(self, output: str, cutoff: float, sample=None):
-        # TODO fill this in
         pass
 
     def subtract(self, sample_1: str, sample_2: str):
@@ -96,6 +54,33 @@ class DelData:
         else:
             return DelData(merged_data)
 
+    def sample_data(self, sample_name: str) -> DelDataSample:
+        sample_data = self.data.iloc[:, ["BB_1", "BB_2", "BB_3", sample_name]]
+        if self.zscored:
+            sample_data.rename({sample_name: "zscore"}, axis=1, inplace=True)
+        else:
+            sample_data.rename({sample_name: "count"}, axis=1, inplace=True)
+        return DelDataSample(sample_data, self.zscored)
+
+
+class DelDataSample(DelData):
+    def merge(self, deldata: DelData) -> DelDataMerge:
+        merged_data = merge(self.data, deldata.data,
+                            on=["BB_1", "BB_2", "BB_3"],
+                            how="outer").fillna(0)
+        return DelDataMerge(merged_data)
+
+    def reduce(self, min_score: float, inplace=False) -> Optional[DelData]:
+        if self.zscored:
+            reduced_data = self.data[self.data.zscore >= min_score]
+        else:
+            reduced_data = self.data[self.data.Count >= min_score]
+        if inplace:
+            self.data = reduced_data
+            return None
+        else:
+            return DelData(reduced_data)
+
 
 def graph_3d(deldata: DelData, out_path: str, min_score: float):
     reduced_data = deldata.reduce(min_score)
@@ -104,9 +89,14 @@ def graph_3d(deldata: DelData, out_path: str, min_score: float):
 
 def read_merge(file_path: str) -> DelData:
     data = read_csv(file_path)
-    return DelData(data)
+    if "Count" in data.columns:
+        raise Exception("Data type is sample. Use delanalysis.read_sample()")
+    return DelDataMerged(data)
 
 
 def read_sample(file_path: str) -> DelData:
     data = read_csv(file_path)
-    return DelData(data)
+    if "Count" not in data.columns:
+        raise Exception(
+            "Data type is not sample. Maybe use delanalysis.read_merge() if it is the merged output data")
+    return DelDataSample(data)
